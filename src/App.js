c536bc1b70f3967e144193cb892ed014adcef951
qvnguyen <nguyenquocviet170898@gmail.com>
2019-08-28 18:23:59 -0400

    Add header component, link pages together

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/hompage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
