e4a330f35876f57cf59d5add76a4228d3f5314c8
qvnguyen <nguyenquocviet170898@gmail.com>
2019-10-27 01:05:08 -0400

    implement cart item reducer, add item to cart action

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -8,7 +8,7 @@ import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import "./cart-icon.styles.scss";
 
 const CartIcon = ({ toggleCartHidden }) => (
-  <div className="cart-icon" onClick={toggleCartHidden}>
+  <div className="cart-icon" onClick={() => toggleCartHidden()}>
     <ShoppingIcon className="shopping-icon" />
     <span className="item-count">0</span>
   </div>
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 import Button from "../button/button.component";
 
+import { connect } from "react-redux";
+import { addItemToCart } from "../../redux/cart/cart.actions";
+
 import "./collection-item.style.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { imageUrl, name, price } = item;
   return (
     <div className="collection-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -12,9 +16,18 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button inverted>Add to cart</Button>
+      <Button onClick={() => addItem(item)} inverted>
+        Add to cart
+      </Button>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItemToCart(item))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CollectionItem);
