110c17b2d2ac6bfbf856fb4602b858f6c74720ab
qvnguyen <nguyenquocviet170898@gmail.com>
2019-11-02 23:53:15 -0400

    create checkout page

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -20,3 +20,12 @@ export const selectCartItemsCount = createSelector(
       0
     )
 );
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumulateQuantity, cartItem) => accumulateQuantity + cartItem.price,
+      0
+    )
+);
