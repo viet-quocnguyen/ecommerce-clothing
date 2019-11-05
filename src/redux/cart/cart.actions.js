af569986ed66387769dacd7fb66fa76ce940d4ff
qvnguyen <nguyenquocviet170898@gmail.com>
2019-11-04 23:42:17 -0500

    add remove item from cart action

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,8 @@ export const addItemToCart = item => ({
   type: CartActionTypes.ADD_ITEM,
   payload: item
 });
+
+export const clearItemFromCart = item => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item
+});
