03c0f7e42254d6ce26b287dac64044f10250c895
viet-quocnguyen <nguyenquocviet170898@gmail.com>
2019-11-20 15:19:26 -0500

    remove middleware from the production environment

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,17 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import { persistStore } from "redux-persist";
-import rootReducer from "./root-reducer";
+import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore } from 'redux-persist';
+import rootReducer from './root-reducer';
 // redux middleware
-import logger from "redux-logger";
+import logger from 'redux-logger';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middlewares = [logger];
+const middlewares = [];
 
-export const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(...middlewares))
-);
+if (process.env.NODE_ENV === 'development') {
+	middlewares.push(logger);
+}
+
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
 
 export const persistor = persistStore(store);
 export default { store, persistor };
