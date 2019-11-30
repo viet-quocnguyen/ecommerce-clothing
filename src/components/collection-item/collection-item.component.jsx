7c399e245e1fc9844db7dea3441736fd19337330
viet-quocnguyen <nguyenquocviet170898@gmail.com>
2019-11-29 23:49:24 -0500

    implement styled-components to collection-item

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -5,21 +5,28 @@ import Button from "../button/button.component";
 import { connect } from "react-redux";
 import { addItemToCart } from "../../redux/cart/cart.actions";
 
-import "./collection-item.style.scss";
+import {
+  ButtonContainer,
+  ImageContainer,
+  CollectionFooterContainer,
+  CollectionItemContainer,
+  NameContainer,
+  PriceContainer
+} from "./collection-item.styles";
 
 const CollectionItem = ({ item, addItem }) => {
   const { imageUrl, name, price } = item;
   return (
-    <div className="collection-item">
-      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
-      <div className="collection-footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
-      <Button className="button" onClick={() => addItem(item)} inverted>
+    <CollectionItemContainer>
+      <ImageContainer imageUrl={imageUrl} />
+      <CollectionFooterContainer>
+        <NameContainer>{name}</NameContainer>
+        <PriceContainer>{price}</PriceContainer>
+      </CollectionFooterContainer>
+      <ButtonContainer onClick={() => addItem(item)} inverted>
         Add to cart
-      </Button>
-    </div>
+      </ButtonContainer>
+    </CollectionItemContainer>
   );
 };
 
---
+++ src/components/collection-item/collection-item.styles.jsx
@@ -0,0 +1,65 @@
+import styled, { css } from "styled-components";
+import Button from "../button/button.component";
+
+// style={{ backgroundImage: `url(${imageUrl})` }}
+
+export const ImageContainer = styled.div`
+  width: 100%;
+  height: 95%;
+  background-size: cover;
+  background-position: center;
+  margin-bottom: 5px;
+  background-image: url('${props => props.imageUrl}');
+`;
+
+export const ButtonContainer = styled(Button)`
+  width: 80%;
+  opacity: 0.7;
+  position: absolute;
+  top: 250px;
+  display: none;
+`;
+
+export const CollectionFooterContainer = styled.div`
+  width: 100%;
+  height: 5%;
+  display: flex;
+  justify-content: space-between;
+  font-size: 18px;
+`;
+
+export const NameContainer = styled.div`
+  width: 90%;
+`;
+
+export const PriceContainer = styled.span`
+  width: 10%;
+`;
+
+export const CollectionItemContainer = styled.div`
+  width: 22vw;
+  min-width: 250px;
+  height: 350px;
+
+  margin-bottom: 20px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  position: relative;
+
+  &:hover ${ImageContainer} {
+    opacity: 0.8;
+  }
+
+  &:hover ${ButtonContainer} {
+    opacity: 0.8;
+    display: flex;
+  }
+
+  @media (max-width: 1000px) {
+    margin: auto;
+    margin-bottom: 20px;
+  }
+`;
